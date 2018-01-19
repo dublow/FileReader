78f58f41b130b20b68a4435a54a895c9fea034d8
dublow <nicolas.dfr@gmail.com>
2018-01-19 17:09:15 +0100

    Red test: A user should be able to read a json file

---
+++ Domain/Implementations/JsonFileReader.cs
@@ -0,0 +1,17 @@
+﻿using Domain.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Domain.Implementations
+{
+    public class JsonFileReader : IFileReader
+    {
+        public IEncryptor Encryptor => throw new NotImplementedException();
+
+        public string Read(string path)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
